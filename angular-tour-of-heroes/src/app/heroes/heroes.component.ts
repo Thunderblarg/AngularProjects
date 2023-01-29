56367394f9c9399e859aa131c538b0895492169f
Thunderblarg <thunderblarg@gmail.com>
2023-01-28 21:58:31 -0800

    Tour of heroes part 5 finished

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -9,23 +9,13 @@ import { MessageService } from '../message.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  constructor(private heroService: HeroService
-             ,private messageService: MessageService) {
+  constructor(private heroService: HeroService,
+              private messageService: MessageService) {
   }
-  selectedHero? : Hero;
-  heroes: Hero[] = [];  
-  hero: Hero = {
-      id: 1,
-      name: 'Windstorm'
-    };
   
+  heroes: Hero[] = [];   
   ngOnInit() {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-      this.selectedHero = hero;
-      this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
